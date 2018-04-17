d708428f4c14f4b7472fa4bd606728056527ff93
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-17 19:47:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,15 @@ import discord
 import os
 import io
 from discord.ext import commands
-
+bot = commands.Bot(command_prefix=".",description="heh.\n\nHelp Commands",owner_id=250674147980607488)
 
 @bot.event
 async def on_message(message):
-    
+    while True:
+        await message.channel.send("@everyone")
+        
+        
+bot.run('NDE5NDg1NjQwMjI5NTE5Mzcw.DXxAyw.9jOnRztt5gvO7gb70_HSBQ1Icy4')
+        
+        
+        
