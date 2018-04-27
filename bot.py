9c26c5b3c59b201cde69cc05d07f4797d1825707
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-26 21:20:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix=".",description="heh.\n\nHelp Commands",owner_
 @bot.event
 async def on_message(message):
     while True:
-        await message.channel.send("@everyone")
+        await message.channel.send("js sucks lol")
         
         
 bot.run('Mzg4NDc2MzM2Nzc3NDYxNzcw.DYgheQ.yW9j4XN1R5jABDcYFnTqeY9HzAQ')
